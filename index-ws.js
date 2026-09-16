f987c01f9d14172ffe31b386094b752784252043
Karim Mourra <12138319+karimMourra@users.noreply.github.com>
2026-09-15 23:12:01 -0300

    creates websocket server and client

--- index-ws.js
+++ index-ws.js
@@ -9,4 +9,30 @@ app.get('/', function(req, resp) {
 server.on('request', app);
 server.listen(3000, function () {
     console.log('kmo express server started on port 3000 what up');
-})
+});
+
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size;
+    console.log('Clients connected: ', numClients);
+
+    wss.broadcast(`Current visitors: ${numClients}`);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome to my server!');
+    }
+
+    ws.on('close', function close() {
+        console.log('A client has disconnected!');
+        wss.broadcast(`Current visitors: ${numClients}`);
+    });
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+}
